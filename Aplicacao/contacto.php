9aaa600580143af90fe9c0910784f6d66b5d481f
jossias <mupandzaj@gmail.com>
2019-07-06 00:34:08 +0200

    validacao de

--- Aplicacao/contacto.php
+++ Aplicacao/contacto.php
@@ -44,14 +44,26 @@
 
     <!-- Acerca -->
     <div class="container">
+        
+        <div class="row my-3 justify-content-center">
+            <div class="col-sm-8 mr-4">
+                <div class="alert alert-success alert-dismissable" id="msg-sucesso">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">            &times;    
+                    </button>    
+                    Mensagen enviada com sucesso!
+                </div>
+            </div>
+        </div>
+        
         <div class="row mb-3 justify-content-center">
             <div class="col-sm-8 mr-4">
                 <h5>Formulário de Contacto</h5>
             </div>
         </div>
+        
         <div class="row mb-5 justify-content-center" id="row-altura">
             <div class="col-sm-8  cor-borda2 cor-creme" id="cadastro">
-                <form action="model/contactoDAO.php" method="POST" class="px-2" id="formulario">
+                <form action="#" method="POST" class="px-2" id="formulario" onsubmit="">
                     <div class="form-row mt-3 justify-content-center">
                         <div class="form-group col-sm-10 ">
                             <label for="Assunto" >Assunto:</label>
@@ -89,82 +101,7 @@
     <?php rodape(); ?>
 
     <!-- Modal caixa de livros-->
-    <div class="modal fade" id="livros" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-            
-                <div class="modal-header">
-                    <h5 class="modal-title">Caixa de Livros</h5>
-                    <button type="button" class="close" data-dismiss="modal">
-                        <span>&times;</span>
-                    </button>
-                </div>
-                
-                <div class="modal-body">
-                    
-                    <div class="container-fluid">
-                    
-                        <div class="row">
-                        
-                            <div class="col-12">
-                                <table class="table table-bordered table-hover col-12 tabela mt-2" id="i2">
-                                    <thead >
-                                        <tr class="cor-creme">
-                                            <td class="titulo-tabela" colspan="5"><i class="fa fa-list mr-2"></i>Lista de livros selecionados</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                        <tr>
-                                            <th class=" text-center" style="width:2px;">
-                                                <input type="checkbox" onclick="selecionar()">
-                                            </th>
-                                            <th class="text-danger">Titulo</th>
-                                            <th class="text-danger">Autor</th>
-                                            <th class="text-danger">Edição</th>
-                                            <th class="text-danger">Ano</th>
-                                        </tr>
-                                    </thead>
-                                            <tbody>
-                                            <tbody id="tabela-corpo"> 
-                                               <tr>
-                                                   <td><input type="checkbox"></td>
-                                                    <td>sfsfdfs</td>
-                                                    <td>sfsfdfs</td>
-                                                     <td>sfsfdfs</td>
-                                                    <td>sfsfdfs</td>
-                                                </tr>
-                                                <tr>
-                                                   <td><input type="checkbox"></td>
-                                                    <td>sfsfdfs</td>
-                                                    <td>sfsfdfs</td>
-                                                     <td>sfsfdfs</td>
-                                                    <td>sfsfdfs</td>
-                                                </tr>
-                                                <tr>
-                                                   <td><input type="checkbox"></td>
-                                                    <td>sfsfdfs</td>
-                                                    <td>sfsfdfs</td>
-                                                     <td>sfsfdfs</td>
-                                                    <td>sfsfdfs</td>
-                                                </tr>
-                                            </tbody>
-                                            </table>
-                            </div>
-                        
-                        </div>
-                    
-                    </div>
-                    
-                </div>
-                
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary text-light">Comfirmar Requisição</button>
-                    <button type="button" class="btn btn-danger text-light">Eliminar</button>
-                </div>
-                
-            </div>
-        </div>
-    </div>
+    <?php modalLivros(); ?>
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- Aplicacao/js/script.js
+++ Aplicacao/js/script.js
@@ -1,6 +1,20 @@
-//valida formularios
-$("#formulario").validate({
-    errorClass: "my-error-class",
-    validClass: "my-valid-class"
+
+$(document).ready(function () {
+    //valida formularios
+    $("#formulario").validate({
+        errorClass: "my-error-class",
+        validClass: "my-valid-class"
+    });
 });
 
+//seleciona todos checkboxes da tabela de livros seleionados (caixa de livros)
+$("#checkbox-livros").click(function () {
+    $('input:checkbox').prop('checked', this.checked);
+});
+
+
+//visualisa mensagen de confirmacao
+/*$("#formulario").submit(function(event) {
+   //alert("dfsdasdad");
+    $("#msg-sucesso").removeClass("d-none"); 
+});*/
