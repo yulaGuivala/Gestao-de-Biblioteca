131afb48ec25af8b9520f38eb9728c94f4c35f15
eddychelene <edmilsonchelene20@gmail.com>
2019-07-13 22:24:03 +0200

    Update index.php

--- Aplicacao/admin/index.php
+++ Aplicacao/admin/index.php
@@ -43,6 +43,9 @@
                   <h4 class="font-weight-bold mb-0">Painel de Controle da Biblioteca</h4>
                 </div>
                 <div>
+                  <h4 class="font-weight-bold mb-0">Estatisticas mensais</h4>
+                </div>
+                <div>
                     <button type="button" class="btn btn-primary btn-icon-text btn-rounded">
                       <i class="ti-clipboard btn-icon-prepend"></i>Imprimir
                     </button>
@@ -54,7 +57,7 @@
             <div class="col-md-3 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <p class="card-title text-md-center text-xl-left">Sales</p>
+                  <p class="card-title text-md-center text-xl-left">Exemplares requisitados</p>
                   <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
                     <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">34040</h3>
                     <i class="ti-calendar icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
