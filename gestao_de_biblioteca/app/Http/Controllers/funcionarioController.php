144ecfafb23cca71f5221f684877523aafa33e50
Folege <follegelricardo@gmail.com>
2019-10-07 22:54:58 +0200

    Actualizando Funcionario

--- gestao_de_biblioteca/app/Http/Controllers/funcionarioController.php
+++ gestao_de_biblioteca/app/Http/Controllers/funcionarioController.php
@@ -8,31 +8,35 @@ Use App\Models\funcionario;
 class funcionarioController extends Controller
 {
     private $funcionario;
-    
+
     public function __construct(Funcionario $funcionario) {
-        
+
         $this->funcionario = $funcionario;
-        
+
     }
     public function create() {
-            
+
+        $dataForm=[
+            'Nome'=>'zzzz',
+        ];
+
         return view('onde serao levados os dados do formularo.');
-        
+
     }
 
 
     public function store(Request $request){
-        
+
         $dataForm = $request->all();
-        
+
         $insert = $this->funcionario->create($dataForm);
-        
+
         return 'Folege';
     }
-    
+
     public function listaFunc(funcionario $funcionario) {
-        
+
         echo $funcionario->all();
-        
+
     }
 }
